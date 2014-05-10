c976adb27ad62d749dd4fa07dae927a23c2ff75b
claudiu <claudiu.garba@gmail.com>
2014-05-10 11:02:07 +0200

    add gin for live reload

--- go/server.go
+++ go/server.go
@@ -3,9 +3,8 @@ package main
 import "github.com/go-martini/martini"
 
 func main() {
-	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello world!"
-	})
-	m.Run()
+  m:= martini.Classic()
+  //magic here
+  m.Run()
+
 }
